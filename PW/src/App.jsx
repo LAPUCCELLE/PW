580c05308d846f7f58c904346f806a2e56b1a80a
LAPUCCELLE <172986073+LAPUCCELLE@users.noreply.github.com>
2025-05-12 13:34:14 -0500

    Agregue más cosas Rodrigo

--- PW/src/App.jsx
+++ PW/src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import React from 'react';
-import Navbar from './Navbar';
+import React from "react";
+import { Routes, Route} from 'react-router-dom';
+import Layout from "./Layout";
 import Mujer from './pages/Mujer';
+
+
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/mujer" element={<Mujer />}/>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="mujer" element={<Mujer />} />
+      </Route>
+    </Routes>
   );
 };
 
