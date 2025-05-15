854fded5be1a276eb3566e3786a03acbb06f0c2a
jotitaf <20215549@aloe.ulima.edu.pe>
2025-05-14 19:26:03 -0500

    Hice mis cambios Jota

---
+++ PW/src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Outlet, Link } from 'react-router-dom';
+import "../login.css"
+
+const Login = () => {
+    <>
+    <main>
+        <div>
+            <h2>Iniciar Sesión </h2>
+        </div>
+    </main>
+    </>
+}
+
+export default Login;
