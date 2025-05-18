c5ec688b6e842de86d6d87ff0b042bc5f5d5cf1b
jotitaf <20215549@aloe.ulima.edu.pe>
2025-05-18 14:47:37 -0500

    Hice mis cambios Jota

---
+++ PW/src/pages/Register.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "../login.css"
+import {Outlet, Link } from 'react-router-dom';
+
+const Register = () => {
+    return (
+        <div className="login-contenedor">
+                  <div className='left-login'>
+                    <img src = './Mujer.png' width={800}/>
+                  </div>
+                  <div className='right-login'>
+                    <div className='login-marco login-marco-register'>
+                      <div className="login-titulo">
+                        <h1>Crea una cuenta</h1>
+                        <p>Empieza a disfrutar de muchos beneficios y a acumular puntos</p>
+                      </div>
+                        <form>
+                          <label for="nombre">Nombre </label><br/>
+                          <input type="nombre" id="nombre" name="nombre" placeholder="Tu nombre" required /><br/>
+                          <label for="email">Correo Electrónico</label><br/>
+                          <input type="email" id="email" name="email" placeholder="Tu correo" required /><br/>
+                          <label for="password">Contraseña</label><br/>
+                          <input type="password" id="password" name="password" placeholder="Crea una contraseña" required /><br/>
+                          <button id="submit" type="submit">Crear cuenta</button>
+                          <hr className="footer-linea" />
+                          <div className="link-register">
+                            ¿Ya tienes una cuenta? <Link to="/login">Inicia sesión</Link>
+                          </div>
+                        </form>
+                    </div>
+                  </div>
+                </div>
+    )
+}
+export default Register;
