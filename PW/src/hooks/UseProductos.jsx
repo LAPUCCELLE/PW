0aab29f433795121fcdac8c321841d2e3ccf0de7
LAPUCCELLE <172986073+LAPUCCELLE@users.noreply.github.com>
2025-05-18 20:35:13 -0500

    Agregué el footer

--- PW/src/hooks/UseProductos.jsx
+++ PW/src/hooks/UseProductos.jsx
@@ -3,11 +3,17 @@ import productosBase from "../data/productos";
 
 export function useProductos() {
     const [productos, setProductos] = useState([])
-
     useEffect(() => {
         const datos = localStorage.getItem("productos")
         if (datos) {
-            setProductos(JSON.parse(datos))
+            const dataParseada = JSON.parse(datos);
+            const ids = new Set(dataParseada.map(p => p.id));
+            if (ids.size === productosBase.length) {
+                setProductos(dataParseada);
+            } else {
+                localStorage.setItem("productos", JSON.stringify(productosBase));
+                setProductos(productosBase);
+            } 
         } else {
             localStorage.setItem("productos", JSON.stringify(productosBase))
             setProductos(productosBase)
